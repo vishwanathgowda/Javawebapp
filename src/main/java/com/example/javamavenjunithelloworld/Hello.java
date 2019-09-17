e94b76575443ccbb9c7f71746ce2cb5714fe044c
Vinay Kumar M <vinay.kumarm@bt.com>
2019-09-17 15:24:22 +0530

    added new features

--- src/main/java/com/example/javamavenjunithelloworld/Hello.java
+++ src/main/java/com/example/javamavenjunithelloworld/Hello.java
@@ -39,6 +39,18 @@ public class Hello {
             printer.println(HELLO);
         }
     }
-    
+    public String sendback(String word)
+    {
+	    System.out.printf("Printing given word %s", word);
+	    return word;
+	    
+    }
+    public int addResult(int a, int b)
+    {
+	    int c = a + b;
+	    return c;
+    }
+	
+	
     
 }
